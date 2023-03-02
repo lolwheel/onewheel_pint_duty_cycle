11969da3c9b4d6d554646c22c7f4901fe0589825
lolwheel <lolwheel@protonmail.com>
2023-03-01 23:17:09 -0800

    Duty cycle patch

--- src/main.cpp
+++ src/main.cpp
@@ -13,17 +13,28 @@ static uint32_t BSS_REQUESTED_SIZE
 static BSS_T* BSS __attribute__((section(".bss_ptr")))
 __attribute__((__used__)) = (BSS_T*)(0x200002B0 + 0x1998);
 
+// From https://github.com/chmike/fpsqrt/blob/master/fpsqrt.c
+static int32_t sqrt_i32(int32_t v) {
+    uint32_t b = 1<<30, q = 0, r = v;
+    while (b > r)
+        b >>= 2;
+    while( b > 0 ) {
+        uint32_t t = q + b;
+        q >>= 1;           
+        if( r >= t ) {     
+            r -= t;        
+            q += b;        
+        }
+        b >>= 2;
+    }
+    return q;
+}
+
 /**
  * This function will be located 8 bytes into the stripped patch. 
  */
 int16_t __attribute__((section(".entry"))) __attribute__((__used__)) patchFunc() {
   int16_t v1 = *(int16_t*)(0x20001bc0);
   int16_t v2 = *(int16_t*)(0x20001bc2);
-  if (v1 < 0) {
-    v1 = - v1;
-  }
-  if (v2 < 0) {
-    v2 = -v2;
-  }
-  return v1;
+  return (sqrt_i32(v1*v1 + v2*v2) * 100) >> 15;
 }
